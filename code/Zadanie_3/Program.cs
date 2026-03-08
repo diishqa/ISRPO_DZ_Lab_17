521d51c2f81fdbecede6b2d568ced9627fc95867
diishqa <diana.34@list.ru>
2026-03-08 20:38:40 +0300

    Zadanie 3

---
+++ code/Zadanie_3/Program.cs
@@ -0,0 +1,22 @@
+﻿Random random = new Random();
+int n = random.Next(1, 101);
+int popitka = 0;
+Console.WriteLine("Угадай число от 1 до 100");
+int ch = 0;
+while (ch != n)
+{
+    Console.Write("Введите число: ");
+    ch = Convert.ToInt32(Console.ReadLine());
+    popitka++;
+    if (ch > n)
+    {
+        Console.WriteLine("Больше");
+    }
+    else if (ch < n)
+    {
+        Console.WriteLine("Меньше");
+    }
+    
+}
+Console.WriteLine("Вы угадали!");
+Console.WriteLine("Попыток: " + popitka);
